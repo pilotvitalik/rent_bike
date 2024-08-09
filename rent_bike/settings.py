423989c08c5210782faf689dd3ada66546c8fbb7
vitaliy_kozhushko <itvkip@yandex.ru>
2024-08-09 16:08:28 +0300

    - созданы приложения проекта - создан requiremets.txt

--- rent_bike/settings.py
+++ rent_bike/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authorization',
+    'bikes',
+    'history_rent',
+    'main',
+    'rent'
 ]
 
 MIDDLEWARE = [
